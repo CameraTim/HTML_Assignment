fa2b27c8c76f2760931dbf2ccd9955e225eed57e
CameraTim <senekerisme@gmail.com>
2022-06-06 00:20:18 -0400

    Add project files.

---
+++ HTML_Assignment/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+var opac = document.getElementById("trans");
+opac.style.opacity = 1;
+opac.style.transition = "opacity 1s";
+
+function transAction() {
+    if (opac.style.opacity == 1) {
+        turnOff()
+    } else {
+        turnOn()
+    }
+
+}
+
+function turnOn() {
+    opac.style.opacity = 1;
+}
+
+function turnOff() {
+    opac.style.opacity = 0;
+}
